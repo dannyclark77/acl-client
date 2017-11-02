46493ff372fe7ff31555c14f82899adf2b63823f
dannyclark77 <dannyclark77@gmail.com>
2017-11-02 16:05:24 -0400

    Create workout model
    - Ran 'ember generate model workout'
    - Added properties of workout model
    - Established belongsTo relationship to user, and hasMany with workouts

---
+++ app/models/workout.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  week: DS.attr('integer'),
+  day: DS.attr('string'),
+  exercise: DS.attr('string'),
+  sets: DS.attr('string'),
+  reps: DS.attr('string')
+  user: DS.belongsTo('user')
+});
