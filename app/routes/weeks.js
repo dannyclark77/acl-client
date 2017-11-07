1900a9e8e10c3ff0ead4aa28c87afc51117f61a7
dannyclark77 <dannyclark77@gmail.com>
2017-11-07 09:16:51 -0500

    Add semicolon

--- app/routes/weeks.js
+++ app/routes/weeks.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   id: Ember.inject.service(),
   actions: {
     submitWeek (week) {
-      let number = week.number
+      let number = week.number;
       this.get('id').set('number', number);
       console.log(this.get('id').number);
       this.transitionTo('week', number);
